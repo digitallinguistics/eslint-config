95fadb139888379a537710d45de73751154bed8e
Daniel W. Hieber <dwhieb@gmail.com>
2024-04-25 17:42:58 -0500

    in progress

--- style.js
+++ style.js
@@ -1,9 +1,28 @@
+import stylistic from '@stylistic/eslint-plugin-js'
+
+// ESLint v1.7.2
 export default {
+  plugins: {
+    '@stylistic/js': stylistic,
+  },
   rules: {
-    'array-bracket-newline': [`error`, `consistent`],
-    'array-element-newline': [`error`, `consistent`],
-    'arrow-parents': [`error`, `as-needed`],
+    '@stylistic/js/function-call-spacing': `error`,
+    'array-bracket-newline':               [`error`, `consistent`],
+    'array-element-newline':               [`error`, `consistent`],
+    'arrow-parens':                        [`error`, `as-needed`],
+    'comma-dangle':                        [`error`, `always-multiline`],
+    'dot-location':                        [`error`, `property`],
+    'function-call-argument-newline':      [`error`, `consistent`],
+    'function-paren-newline':              `error`,
+    'generator-star-spacing':              `error`,
+    'implicit-arrow-linebreak':            `error`,
+    'indent':                              [`error`, 2],
+    'key-spacing':                         [`error`, {
+      align: `value`,
+      mode:   `minimum`,
+    }],
+    quotes:         [`error`, `backtick`],
     'semi-spacing': `error`,
-    'semi-style': `error`,
+    'semi-style':   `error`,
   },
 }
